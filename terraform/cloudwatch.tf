32f0da10d8d93cd2a9d8c0898b4ec1e676a10012
Matt Leonhardt <matthewleonhardt@outlook.com>
2025-03-22 11:31:12 -0700

    Add API gateway

--- terraform/cloudwatch.tf
+++ terraform/cloudwatch.tf
@@ -1,6 +1,13 @@
 resource "aws_cloudwatch_log_group" "tesla_callback_fn" {
+    provider = aws.us-west-1
     name = "/aws/lambda/tesla-callback-fn"
 
     retention_in_days = 30
 }
 
+resource "aws_cloudwatch_log_group" "tesla_callback_api" {
+    provider = aws.us-west-1
+    name = "/aws/apigateway/tesla-callback-api"
+
+    retention_in_days = 30
+}
