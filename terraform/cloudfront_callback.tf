fb88b78096cb48f2fdf5336ff08daee2e9318198
itt-mattleonhardt <matt.leonhardt@intimetec.com>
2025-03-22 12:57:23 -0700

    Put API Gateway behind cloudfront and update DNS

---
+++ terraform/cloudfront_callback.tf
@@ -0,0 +1,64 @@
+resource "aws_cloudfront_cache_policy" "tesla_callback" {
+  provider = aws.us-east-1
+  name     = "tesla-callback"
+  min_ttl  = 1
+  parameters_in_cache_key_and_forwarded_to_origin {
+    cookies_config {
+      cookie_behavior = "none"
+    }
+    headers_config {
+      header_behavior = "none"
+    }
+    query_strings_config {
+      query_string_behavior = "all"
+    }
+  }
+}
+
+resource "aws_cloudfront_distribution" "tesla_callback_distribution" {
+  provider = aws.us-east-1
+  origin {
+    domain_name = "${aws_apigatewayv2_api.tesla_callback.id}.execute-api.us-west-1.amazonaws.com"
+    origin_id   = aws_apigatewayv2_api.tesla_callback.id
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "https-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled         = true
+  is_ipv6_enabled = true
+  aliases         = ["tesla-api.mattleonhardt.codes"]
+  price_class     = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      locations        = []
+      restriction_type = "none"
+    }
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = aws_apigatewayv2_api.tesla_callback.id
+
+    cache_policy_id            = aws_cloudfront_cache_policy.tesla_callback.id
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.security_headers.id
+    viewer_protocol_policy     = "redirect-to-https"
+  }
+
+  viewer_certificate {
+    acm_certificate_arn = aws_acm_certificate.tesla_app.arn
+    ssl_support_method  = "sni-only"
+  }
+
+  logging_config {
+    bucket          = "${data.aws_s3_bucket.logs.bucket}.s3.amazonaws.com"
+    include_cookies = false
+    prefix          = "tesla-app/cloudfront-callback/"
+  }
+}
--- terraform/route53.tf
+++ terraform/route53.tf
@@ -8,11 +8,23 @@ data "aws_route53_zone" "mlcodes" {
 resource "aws_route53_record" "tesla_app" {
     provider = aws.us-west-1
     zone_id = data.aws_route53_zone.mlcodes.zone_id
-    name = "mattleonhardt.codes"
+    name = "tesla.mattleonhardt.codes"
     type = "A"
     alias {
         name = aws_cloudfront_distribution.tesla_app_distribution.domain_name
         zone_id = aws_cloudfront_distribution.tesla_app_distribution.hosted_zone_id
+        evaluate_target_health = false
+    }
+}
+
+resource "aws_route53_record" "tesla_callback" {
+    provider = aws.us-west-1
+    zone_id = data.aws_route53_zone.mlcodes.zone_id
+    name = "tesla-api.mattleonhardt.codes"
+    type = "A"
+    alias {
+        name = aws_cloudfront_distribution.tesla_callback_distribution.domain_name
+        zone_id = aws_cloudfront_distribution.tesla_callback_distribution.hosted_zone_id
         evaluate_target_health = false
     }
 }
