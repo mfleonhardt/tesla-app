619bd25ed50888639d9590e408738fca9e683385
Matt Leonhardt <matthewleonhardt@outlook.com>
2025-03-30 12:15:31 -0700

    DevX: Set up local cert and subdomain

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
+import dotenv from 'dotenv'
+
+dotenv.config({ path: '.env.local' });
+
+const defaultCertPath = path.resolve(__dirname, '../certs');
+const host = process.env.VITE_DEV_HOST || 'localhost';
+const keyPath = process.env.VITE_KEY_PATH || path.resolve(defaultCertPath, 'app-key.pem')
+const certPath = process.env.VITE_CERT_PATH || path.resolve(defaultCertPath, 'app-cert.pem')
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -8,4 +18,13 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
-})
+  server: {
+    host,
+    port: 3000,
+    https: {
+      key: keyPath,
+      cert: certPath,
+  },
+  },
+});
+
---
+++ terraform/outputs.tf
@@ -0,0 +1,7 @@
+output "tesla_app_subdomain" {
+  value = var.tesla_app_subdomain
+}
+
+output "tesla_callback_subdomain" {
+  value = var.tesla_callback_subdomain
+}
